3b956c1d33d63b3233324a93535532e00a0a8a04
AntiPetrun <apetrunikov@tutanota.com>
2023-04-05 13:22:05 +0300

    modified test_items.py: assert checking

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ def test_button_add_to_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(30)
     button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    assert button.get_attribute('value') == button.text, 'Button attribute doesn\'t match value'
     assert button, 'Button doesn\'t exist'
+    assert button.get_attribute('value') == button.text, 'Button attribute doesn\'t match value'
     button.click()
